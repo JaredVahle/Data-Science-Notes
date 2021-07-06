53fb983f8478336db9b4319f6fb86897faa9ad13
JaredVahle <jaredjohnvahle@gmail.com>
2021-07-06 09:57:11 -0500

    Pushing temp table notes

--- temp_tables_notes.sql
+++ temp_tables_notes.sql
@@ -5,7 +5,7 @@
 CREATE TEMPORARY TABLE table_name(...);
 
 # --- TEMPORARY TABLES CAN BE REMOVED WITH THE (DROP TABLE STATEMENT)
-DROP TABLE;
+DROP TABLE example_table_name;
 
 #-- QUERY TO CREATE A TEMP TABLE
 
@@ -13,7 +13,11 @@ CREATE TEMPORARY TABLE my_numbers(
 	n INT UNSIGNED NOT NULL
 );
 
+DESCRIBE my_numbers;
+
+
 # NOW WE WILL INSERT SOME DATA INTO IT.
+# INSERT IS HOW WE ADD RECORDS/ROWS TO A TABLE
 
 INSERT INTO my_numbers(n) 
 VALUES (1), (2), (3), (4), (5);
@@ -23,15 +27,26 @@ VALUES (1), (2), (3), (4), (5);
 SELECT *
 FROM my_numbers;
 
+
+# DB OPERATOPNS
+# CRUD
+# CREATE, READ, UPDATE, DELETE
+
+
 # UPDATE AKA. MODIFY INFORMATION IN THE TABLE
 
 UPDATE my_numbers 
-SET n = n + 1;
+SET n = n + 20;
+
+SELECT *
+FROM my_numbers;
 
 # BECAUSE THERE IS NO WHERE CLAUSE THE UPDATE WILL APPLY TO ALL ROWS IN THE TABLE
+# DELETE IS PRETTY DESTRUCTIVE
+# ALWAYS START YOUR DELETE STATEMENT AS IF IT WERE A SELECT (since the syntax is similar)
 
 DELETE FROM my_numbers 
-WHERE n % 2 = 0
+WHERE n % 2 = 0;
 
 # TO SEE RESULTS
 
@@ -47,7 +62,6 @@ JOIN dept_emp USING(emp_no)
 JOIN departments USING(dept_no)
 LIMIT 100;
 
-
 # -----------Updating table structure--------
 #ALTER TABLE
 # IF WE WANTED TO DROP THE DEPT_NO
@@ -57,3 +71,50 @@ ALTER TABLE employees_with_departments ADD email VARCHAR(100);
 # AFTER ADDING A COLUMN THE NUMBERS WILL BE NULL, WE CAN POPULATE THAT COLUMN WITH DaTE BY RUNNING AN UPDATE:
 UPDATE employees_with_departments
 SET email = CONCAT(first_name, "@company.com");
+
+# CREATING A TEMP TABLE FROM A QUERY
+SELECT DATABASE();
+
+CREATE TEMPORARY TABLE georgis AS
+SELECT *
+FROM employees.employees -- db_name.table_name
+JOIN employees.salaries USING (emp_no)
+WHERE first_name = "Georgi";
+
+SELECT *
+FROM georgis;
+
+
+# ANOTHER WAY TO WRITE IT
+USE employees;
+
+CREATE TEMPORARY TABLE germain_1460.partos AS
+SELECT *
+FROM employees
+JOIN salaries USING(emp_no)
+WHERE first_name = "Parto";
+
+-- FROM HERE, WE NEED TO DO db_name.table_name
+SELECT * 
+FROM germain_1460.partos;
+
+# ALTER REMOVED emp_no
+ALTER TABLE germain_1460.partos DROP column emp_no;
+
+ALTER TABLE germain_1460.partos add email VARCHAR(200);
+
+UPDATE germain_1460.partos SET email = concat(first_name, ".", last_name, "@company.com");
+
+SELECT *
+FROM germain_1460.partos;
+
+
+USE germain_1460;
+CREATE TEMPORARY TABLE numbers AS
+SELECT *
+FROM numbers.numbers;
+
+-- SHOWS WHAT DATABASE IM USING
+SELECT DATABASE();
+SELECT * FROM numbers;
+
